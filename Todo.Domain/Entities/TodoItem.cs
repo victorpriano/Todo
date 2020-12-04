1f3ac3b49cf4bf3692666b266d14db815a42f491
Victor <victorcuru@gmail.com>
2020-12-04 11:48:57 -0300

    Criando entidades

---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string title, bool done, DateTime date, string user)
+        {
+            Title = title;
+            Done = done;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; } // RefUser
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+
+
+    }
+}
